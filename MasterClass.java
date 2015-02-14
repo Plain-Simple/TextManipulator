1be61a27a5f987d1334c7544d945f1cef076b930
Matthew <unsolvedcypher@gmail.com>
2015-02-14 16:34:31 -0500

    minor refactoring

--- MasterClass.java
+++ MasterClass.java
@@ -1,5 +1,18 @@
-/**
- * Created by mmcmillan on 2/13/15.
- */
-public class MasterClass {
+class MasterClass {
+
+    public static void main(String args[]) {
+        MasterClass masterClass = new MasterClass();
+        masterClass.StartProgram();
+    }
+    private void StartProgram() {
+        boolean RunGUI = true;
+        /* set this to false to run in CLI mode. There will be a better way
+         * to do this in the future */
+        if (RunGUI) {
+            GUI gui = new GUI();
+            gui.StartGUI();
+        }
+        CLI cli = new CLI();
+        cli.StartCLI();
+    }
 }

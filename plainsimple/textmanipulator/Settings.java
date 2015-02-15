f76f8dc8f33f74e83a317ae5b502215d79090ce7
stefan4472 <stefan.kussmaul@gmail.com>
2015-02-15 18:02:04 -0500

    Supports Accent Buttons

--- plainsimple/textmanipulator/Settings.java
+++ plainsimple/textmanipulator/Settings.java
@@ -1,76 +1,95 @@
 package plainsimple.textmanipulator;
 
 import java.io.*;
+import java.util.ArrayList;
 
 /* Plain+Simple TextManipulator settings and global variable management */
-class Settings {
-    public static void DefaultSettings(boolean analyzetext_settings[]) {
+public class Settings {
+    private ArrayList<Integer> settings = new ArrayList<Integer>(); // work in progress
+    public ArrayList<Integer> getSettings() {return settings;}
+    public void setDefaultSettings(String file_name) {
         try {
-            FileWriter file = new FileWriter("TextManipulator_Settings");
+            FileWriter file = new FileWriter(file_name);
             BufferedWriter write_settings = new BufferedWriter(file);
-            write_settings.write("#Line 1: AnalyzeText Settings: WordCount, CharCount, LineCount, SentenceCount, WordFrequency, CharFrequency\n");
-            write_settings.write("1,1,1,1,1,1\n"); /* sets values on file to true by default */
+            write_settings.write("130");
+            write_settings.newLine();
+            write_settings.write("131");
+            write_settings.newLine();
+            write_settings.write("132");
+            write_settings.newLine();
+            write_settings.write("133");
+            write_settings.newLine();
+            write_settings.write("134");
+            write_settings.newLine();
+            write_settings.write("135");
+            write_settings.newLine();
+            write_settings.write("136");
+            write_settings.newLine();
+            write_settings.write("137");
+            write_settings.newLine();
             write_settings.close();
         } catch (IOException e) {
-            //Print("Error writing default settings file\n");
+            System.out.println("Error writing default settings");
         }
-        for(int i = 0; i < 6; i++)
-            analyzetext_settings[i] = true; /* set all values to true by default */
+        settings.add(130);
+        settings.add(131);
+        settings.add(132);
+        settings.add(133);
+        settings.add(134);
+        settings.add(135);
+        settings.add(136);
+        settings.add(137);
     }
-
-    public static boolean LoadSettings(boolean analyzetext_settings[]) {
-        boolean load = true;
+    /* loads settings from file name. Creates default settings if an exception is found */
+    public boolean loadSettings(String file_name) {
+        boolean read_success = true;
         try {
-            FileReader file = new FileReader("TextManipulator_Settings");
+            FileReader file = new FileReader(file_name);
             BufferedReader read_settings = new BufferedReader(file);
-            String line;
-            int line_counter = 1;
+            String line = "";
             while((line = read_settings.readLine()) != null) {
-                if(line_counter == 1) {/* line 1: load settings for AnalyzeText */
-                    /* line is "1,1,1,1,1,1" so we can read it in easily. Each value is parsed from String to boolean for easy use */
-                    analyzetext_settings[0] = CharToBoolean(line.charAt(0));
-                    analyzetext_settings[1] = CharToBoolean(line.charAt(2));
-                    analyzetext_settings[2] = CharToBoolean(line.charAt(4));
-                    analyzetext_settings[3] = CharToBoolean(line.charAt(6));
-                    analyzetext_settings[4] = CharToBoolean(line.charAt(8));
-                    analyzetext_settings[5] = CharToBoolean(line.charAt(10));
-                }
-                line_counter++;
+                settings.add(Integer.parseInt(line));
             }
+            System.out.println("File \"" + file_name + "\" loaded");
+            System.out.println(settings.toString());
         } catch (IOException e) {
-            //Print("Error reading settings file\n");
-            load = false;
+            System.out.println("Error reading settings file\n");
+            read_success = false;
+            setDefaultSettings(file_name);
         }
-        return load; /* returns whether "TextManipulator_Settings" was accessed and read */
+        return read_success; /* returns whether "TextManipulator_Settings" was accessed and read */
     }
     /* updates "TextManipulator_Settings" with new values from program - will be run every time the user changes settings */
-    public static boolean UpdateSettings(boolean[] analyzetext_settings) {
-        boolean success = true;
+    public boolean updateSettings(String file_name) {
+        boolean write_success = true;
         try {
-            FileWriter file = new FileWriter("TextManipulator_Settings");
+            FileWriter file = new FileWriter(file_name);
             BufferedWriter write_settings = new BufferedWriter(file);
-            /* write settings to file (in the correct order), first converting each value to a char and comma-separating them */
-            write_settings.write(BooleanToChar(analyzetext_settings[0]) + "," + BooleanToChar(analyzetext_settings[1]) +
-                    "," + BooleanToChar(analyzetext_settings[2]) + "," + BooleanToChar(analyzetext_settings[3]) + "," +
-                    BooleanToChar(analyzetext_settings[4]) + "," + BooleanToChar(analyzetext_settings[5]));
-            write_settings.newLine();
+            for(int i = 0; i < settings.size(); i++) {
+                write_settings.write(Integer.toString(settings.get(i)));
+                write_settings.newLine();
+            }
             write_settings.close();
         } catch(IOException e) {
             //Print("Error writing to file\n");
-            success = false;
+            write_success = false;
         }
-        return success;
+        return write_success;
     }
     /* returns false if '0', true if anything else. Used for transferring variables from "TextManipulator_Settings to
  * the program */
-    private static boolean CharToBoolean(char c) {
-        return c != '0';
+    public boolean CharToBoolean(char c) {
+        if(c == '0')
+            return false;
+        else
+            return true;
     }
     /* returns '0' if false, '1' if true. Used for transferring variables from program to "TextManipulator_Settings" */
-    private static char BooleanToChar(boolean b) {
-        if(!b)
+    public char BooleanToChar(boolean b) {
+        if(b == false)
             return '0';
         else
             return '1';
     }
 }
+

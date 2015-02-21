2943fe6c1b5ae2610c39289263e4a77a91324954
stefan4472 <stefan.kussmaul@gmail.com>
2015-02-21 13:49:58 -0500

    changeDirectory and getFilesInDirectory

--- plainsimple/textmanipulator/Directory.java
+++ plainsimple/textmanipulator/Directory.java
@@ -1,7 +1,11 @@
 package plainsimple.textmanipulator;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Directory {
     private Path path;
@@ -15,4 +19,32 @@ public class Directory {
     }
     public Path getParent() {return path.getParent();}
     public void setAsParent() {path = path.getParent();}
+    public ArrayList<TextFile> getFilesInDirectory() {
+        ArrayList<TextFile> files_in_directory = new ArrayList<>();
+        try {
+            File[] matchingFiles = new File(path.toString()).listFiles(new FileFilter() {
+                @Override public boolean accept(File dir) {
+                    return dir.isFile();
+                } // how to get only text files?
+            });
+            for (int i = 0; i < matchingFiles.length; i++) {
+                files_in_directory.add(new TextFile(matchingFiles[i].toPath()));
+            }
+        } catch(SecurityException e) { // ToDo: better handling and more catch statements
+            System.out.println("Error: Could not access files due to Security Manager.");
+        }
+        return files_in_directory;
+    }
+    /* attempts to change directory to new_directory. If the new directory is valid,
+    it will copy the path and return true; otherwise, it will ignore the new directory
+    and return false. */
+    public boolean changeDirectory(String new_directory) {
+        boolean change_success = false;
+        Directory test_directory = new Directory(Paths.get(new_directory));
+        if(test_directory.directoryExists()) {
+            path = test_directory.getPath();
+            return true;
+        }
+        return change_success;
+    }
 }

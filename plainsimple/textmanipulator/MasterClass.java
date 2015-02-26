334f91b075335d574afe78a2a056cbaab2c3d553
Matthew <unsolvedcypher@gmail.com>
2015-02-26 17:21:37 -0500

    rewrote code to decide interface

--- plainsimple/textmanipulator/MasterClass.java
+++ plainsimple/textmanipulator/MasterClass.java
@@ -3,16 +3,21 @@ package plainsimple.textmanipulator;
 class MasterClass {
   public static void main(String args[]) {
     i18n.setUpi18n(args);
-    /* start the actual program: */
-    boolean RUN_GUI = true;
-    /* set this to false to run in CLI mode. There will be a better way
-     * to do this in the future */
-    if (RUN_GUI) {
+    /* 0 = CLI, 1 = JavaFX, 2 = Swing
+
+     * do not rely on Swing, it is no
+     * longer in development and will
+     * be discontinued by us soon  */
+
+    int RUN_INTERFACE = 0;
+    if (RUN_INTERFACE == 0) {
+      CLI cli = new CLI();
+      cli.startCLI();
+    } if (RUN_INTERFACE ==1) {
+      JavaFXGUI.launch(JavaFXGUI.class, args);
+    } if (RUN_INTERFACE == 2) {
       GUI gui = new GUI();
       gui.startGUI();
-      //JavaFXGUI.launch(JavaFXGUI.class, args);
     }
-    CLI cli = new CLI();
-    cli.startCLI();
   }
 }

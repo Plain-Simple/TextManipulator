a0ee0521c110013dfcb8d8cae1fe2ff91ea6e26f
stefan4472 <stefan.kussmaul@gmail.com>
2015-02-28 22:34:02 -0500

    File Exists, Paste Functions

--- plainsimple/textmanipulator/TextFile.java
+++ plainsimple/textmanipulator/TextFile.java
@@ -1,5 +1,10 @@
 package plainsimple.textmanipulator;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -26,9 +31,10 @@ public class
     file_name = name;
     file_path = Paths.get(path).toAbsolutePath();
   }
+    /* tries to create a bufferedreader object using this file's path */
   public boolean fileExists() {
     try {
-      file_path.toRealPath();
+        BufferedReader test_reader = Files.newBufferedReader(file_path.toRealPath());
     } catch(IOException e) {
       return false;
     }
@@ -59,6 +65,7 @@ public class
     }
     return true;
   }
+    /* writes file_text object to file */
   public boolean writeFile() {
     try (BufferedWriter writer = Files.newBufferedWriter(file_path)) {
       writer.write(file_text, 0, file_text.length());
@@ -72,4 +79,27 @@ public class
     boolean delete_success = file_to_delete.delete();
     return delete_success;
   }
+    public boolean pasteIntoFile() {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        /* get contents from clipboard (stored in a Transferable, which manages data transfer) */
+        Transferable contents = clipboard.getContents(null);
+        /* if contents are transferable, the Transferable will not be null and will be the
+        correct DataFlavor (String). DataFlavor refers to the type of object something is */
+        boolean hasTransferableText =
+                (contents != null) && contents.isDataFlavorSupported(DataFlavor.stringFlavor);
+        if (hasTransferableText) {
+            try {
+                String clipboard_contents = (String)contents.getTransferData(DataFlavor.stringFlavor);
+                file_text = clipboard_contents;
+                boolean write_success = writeFile();
+                return write_success; /* returns true or false for whole operation */
+            } catch (UnsupportedFlavorException | IOException ex) {
+                System.out.println("Error: Could not paste contents into file. Operation aborted.");
+                return false;
+            }
+        } else {
+            System.out.println("Error: Clipboard is empty or does not contain writable text.");
+            return false;
+        }
+    }
 }

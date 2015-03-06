8e0381d3ef49264d85db54eb559bbb5633a5235c
stefan4472 <stefan.kussmaul@gmail.com>
2015-03-05 21:51:43 -0500

    Improved Word Count, Frequency Functions

--- plainsimple/textmanipulator/AnalyzeText.java
+++ plainsimple/textmanipulator/AnalyzeText.java
@@ -4,13 +4,23 @@
 
 package plainsimple.textmanipulator;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class AnalyzeText {
+    /* regex pattern to find words */
+    private Pattern word_pattern = Pattern.compile("\\w+");
   public int CharCount(String text) {
     return text.replace("\n", "").length();
   }
   public int WordCount(String text) {
-    return text.length()
-           - text.replace(" ", "").replace("\n", "").length() + 1;
+      Matcher matcher = word_pattern.matcher(text);
+      int instances = 0;
+      while(matcher.find()) {
+         instances++;
+      }
+      return instances;
   }
   public int lineCount(String text) {
     return text.length() - text.replace("\n", "").length() + 1;
@@ -25,9 +35,38 @@ class AnalyzeText {
     return sentence_count;
   }
 
-  public void WordFrequency(String text) {
+  public void WordFrequency(String text) { // ToDo: remains to be tested
+      String[] words = text.split("\\w+");
+      ArrayList<String> unique_words = new ArrayList<>();
+      ArrayList<Integer> frequencies = new ArrayList<>();
+      for(int i = 0; i < words.length; i++) {
+          /* get location of current word in list */
+          int index = unique_words.indexOf(words[i]);
+          if(index < 0) {
+              /* add word to list */
+              unique_words.add(words[i]);
+              frequencies.add(0);
+          } else {
+              /* add one to correct index */
+              frequencies.set(index, frequencies.get(index) + 1);
+          }
+      }
   }
 
   public void CharFrequency(String text) {
+      ArrayList<Character> unique_chars = new ArrayList<>();
+      ArrayList<Integer> frequencies = new ArrayList<>();
+      for(int i = 0; i < text.length(); i++) {
+          /* get location of current word in list */
+          int index = unique_chars.indexOf(text.charAt(i));
+          if(index < 0) {
+              /* add char to list */
+              unique_chars.add(text.charAt(i));
+              frequencies.add(0);
+          } else {
+              /* add one to correct index */
+              frequencies.set(index, frequencies.get(index) + 1);
+          }
+      }
   }
 }

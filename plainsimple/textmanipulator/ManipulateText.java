cbf9d5f8429f18eba595de165ef2ec013cf0a1a4
stefan4472 <stefan.kussmaul@gmail.com>
2015-03-23 20:39:39 -0400

    Restructuring to Accomodate Shell

--- plainsimple/textmanipulator/ManipulateText.java
+++ plainsimple/textmanipulator/ManipulateText.java
@@ -16,7 +16,13 @@ class ManipulateText {
   /* adds prefix and suffix to each line */
   @SuppressWarnings("HardCodedStringLiteral")
   /* create an array that holds each individual line */
-  public String[] splitIntoLines(String text) {
+  public String[] splitIntoLines(String text) { // todo: non-destructive split. One array for objects, one for delimiters
+      // todo: that can later be merged
+      Pattern lines_pattern = Pattern.compile("\\r?\\n");
+    //  Matcher matcher = lines_pattern.matcher(text);
+    //  while (matcher.find()) {
+
+    //  }
       return text.split("\\r?\\n");
   }
     public String[] splitIntoWords(String text) {
@@ -25,18 +31,23 @@ class ManipulateText {
     public String[] splitIntoChars(String text) {
         return text.split(".");
     }
-
-  public String addPrefixSuffix(String[] text, String prefix, String suffix) {
-      String result = "";
+    /* puts each individual sentence on a separate line */
+    public String[] splitIntoSentences(String text) { // todo: fix this function
+        return text.split("(\\d+)");
+    }
+    public String[] getAsArray(String text) {
+        return new String[] {text};
+    }
+  public String[] addPrefixSuffix(String[] text, String prefix, String suffix) {
     /* add each line to text with prefix and suffix */
     for(int i = 0; i < text.length; i++)
-      result += prefix + text[i] + suffix + "\n";
-    return result;
+      text[i] = prefix + text[i] + suffix + "\n";
+    return text;
   }
   @SuppressWarnings("HardCodedStringLiteral")
-  public String removeDuplicateObjects(String text[]) {
+  public String[] removeDuplicateObjects(String text[]) {
     ArrayList<Integer> duplicates = new ArrayList<>();
-      String result = "";
+    ArrayList<String> result = new ArrayList<>();
     for(int i = 0; i < text.length; i++) {
       for(int j = i + 1; j < text.length;
           j++) { /* check to see if any of the later elements match */
@@ -51,10 +62,10 @@ class ManipulateText {
           copy_element = false;
       }
       if(copy_element) {
-          result += text[i];
+          result.add(text[i]);
       }
     }
-    return result;
+    return result.toArray(new String[result.size()]); /* return String array */
   }
   @SuppressWarnings("HardCodedStringLiteral")
   public String removeObjectsContaining(String text[], String remove) {
@@ -66,30 +77,27 @@ class ManipulateText {
     return result;
   }
   @SuppressWarnings("HardCodedStringLiteral")
-  public String scrambleObjects(String text[]) {
-      String result = "";
-    ArrayList<String> lines_list = new ArrayList<>(Arrays.asList(text));
-    while(lines_list.size() > 0) { /* runs until all lines have been copied */
-      int line_number = generateRandomNumber(lines_list.size() -
+  public String[] scrambleObjects(String text[]) {
+    String[] result = new String[text.length];
+    ArrayList<String> objects_list = new ArrayList<>(Arrays.asList(text));
+    int counter = 0;
+    while(objects_list.size() > 0) { /* runs until all lines have been copied */
+      int line_number = generateRandomNumber(objects_list.size() -
                                              1); /* generate random number within range of list */
-      result += lines_list.get(line_number) +
-             "\n"; /* copy corresponding element to text */
-      lines_list.remove(
-        line_number); /* remove the element from the list so it cannot be copied again */
+      result[counter] = objects_list.get(line_number); /* copy corresponding element to text */
+      objects_list.remove(
+              line_number); /* remove the element from the list so it cannot be copied again */
+      counter++;
     }
     return result;
   }
   @SuppressWarnings("HardCodedStringLiteral")
-  public String sortObjectsAlphabetically(String text[]) {
-    String result = "";
+  public String[] sortObjectsAlphabetically(String text[]) {
     Arrays.sort(text);
-    for(int i = 0; i < text.length; i++)
-      result += text[i] + "\n";
-    return result;
+    return text;
   }
   @SuppressWarnings("HardCodedStringLiteral")
-  public String sortObjectsBySize(String text[]) {
-    String result = "";
+  public String[] sortObjectsBySize(String text[]) {
     for(int i = 1; i < text.length; i++) { /* start at second element */
       int num = 1;
       String compare = text[i];
@@ -101,49 +109,44 @@ class ManipulateText {
         num++;
       }
     }
-    for(int i = 0; i < text.length; i++) {
-      result += text[i] + "\n";
-    }
-    return result;
+    return text;
   }
   @SuppressWarnings("HardCodedStringLiteral")
-  public String numberObjects(String text[], String prefix, String suffix) {
-    String result = "";
+  public String[] numberObjects(String text[], String prefix, String suffix) {
     for(int i = 0; i < text.length; i++) {
-      result += prefix + (i + 1) + suffix + text[i] + "\n";
+      text[i] = prefix + (i + 1) + suffix + text[i] + "\n";
     }
-    return result;
+    return text;
   }
   @SuppressWarnings("HardCodedStringLiteral")
-  public String mergeText(String text[], String merge[]) {
-    String result = "";
-    for(int i = 0; i < text.length; i++) {
-      if(i < merge.length) { /* will prevent merge_lines from going out of bounds if it is smaller than text */
-        result += text[i] + merge[i] + "\n";
-      } else {
-        result  += text[i] +
-                   "\n";  /* all elements of lines2 have been transferred */
-      }
+  public String[] mergeText(String text[], String merge[]) {
+    if(text.length >= merge.length) {
+          for(int i = 0; i < merge.length; i++) {
+              text[i] = text[i] + merge[i];
+          }
+        return text;
     }
-    if(merge.length > text.length) {
-      for(int i = text.length; i < merge.length; i++) {
-      /* merge any remaining elements from merge_lines */
-        result += merge[i] + "\n";
+    else { /* merge.length > text.length */
+        String[] result = merge;
+      for(int i = 0; i < text.length; i++) {
+          result[i] = text[i] + merge[i];
       }
+        return result;
     }
-    return result;
   }
-  public String findReplace(String text, String find, String replace) { /* use String.replaceAll(find, replace) */
-      String result = ""; // is matcher.group() necessary?
+  public String[] findReplace(String text[], String find, String replace) { /* use String.replaceAll(find, replace) */
+      String[] result = text;
       try {
           Pattern expression_to_find = Pattern.compile(find);
-          Matcher matcher = expression_to_find.matcher(text);
           int instances = 0;
           boolean found = false;
-          while(matcher.find()) {
-              result = text.substring(0, matcher.start()) + text.substring(matcher.end());
-              instances++;
-              found = true;
+          for(int i = 0; i < text.length; i++) {
+              Matcher matcher = expression_to_find.matcher(text[i]);
+              while (matcher.find()) {
+                  result[i] = text[i].substring(0, matcher.start()) + text[i].substring(matcher.end());
+                  instances++;
+                  found = true;
+              }
           }
           if(!found)
               Println("No instances found.");
@@ -155,8 +158,11 @@ class ManipulateText {
     return result;
   }
   /* removes all instances of 'argument' from 'text' */
-  public String removeArgument(String text, String argument) {
-    return text.replaceAll(argument, "");
+  public String[] removeArgument(String text[], String argument) {
+      for(int i = 0; i < text.length; i++) {
+          text[i].replaceAll(argument, "");
+      }
+      return text;
   }
   public String commaSeparateValues(String text) {
       return removePunctuation(removeExtraWhitespace(text)).replace(' ', ',');
@@ -201,28 +207,25 @@ class ManipulateText {
             System.out.println("Error: System clipboard is currently unavailable.");
         }
     }
-    public void find(String text, String regex) {
+    public void find(String text[], String regex) {
         try {
             Pattern expression_to_find = Pattern.compile(regex);
-            Matcher matcher = expression_to_find.matcher(text);
             int instances = 0;
             boolean found = false;
-            while(matcher.find()) {
-                Println("Instance found at " + matcher.start());
-                instances++;
-                found = true;
+            for (int i = 0; i < text.length; i++) {
+                Matcher matcher = expression_to_find.matcher(text[i]);
+                while (matcher.find()) {
+                    Println("Instance found at " + matcher.start());
+                    instances++;
+                    found = true;
+                }
             }
-            if(found) {
+            if(found)
                 Println(instances + " of expression found.");
-            }
-        } catch(PatternSyntaxException e) {
+        }catch(PatternSyntaxException e) {
             Println("Error: Expression to find is invalid (regex).");
         }
     }
-  /* puts each individual sentence on a separate line */
-  public String[] splitIntoSentences(String text) { // todo: fix this function
-      return text.split("(\\d+)");
- }
   /* generates a random number between 0 and 'upper_bound' inclusive */
   int generateRandomNumber(int
                            upper_bound) { /// I'm pretty sure there's a java.util.Random library we can use
@@ -230,7 +233,7 @@ class ManipulateText {
     return (int) Math.floor(Math.random() * (upper_bound + 1));
   }
   public String removeExtraWhitespace(String text) { // note: removes tabs and linebreaks
-    return text.replaceAll("\\s+", " ").trim();
+      return text.replaceAll("\\s+", " ").trim();
   }
   /* removes all non-letter and non-numbers, leaves spaces */
   public String removePunctuation(String text) {
@@ -238,16 +241,20 @@ class ManipulateText {
   }
 
 
-  public String forceUppercase(String text) {
-    return text.toUpperCase();
+  public String[] forceUppercase(String text[]) {
+    for(int i = 0; i < text.length; i++)
+        text[i] = text[i].toUpperCase();
+      return text;
   }
-  public String forceLowercase(String text) {
-    return text.toLowerCase();
+  public String[] forceLowercase(String text[]) {
+    for(int i = 0; i < text.length; i++)
+        text[i] = text[i].toLowerCase();
+      return text;
   }
   public void Print(String s) {
-    System.out.print(s);
+      System.out.print(s);
   }
   public void Println(String s) {
-    System.out.println(s);
+      System.out.println(s);
   }
 }
